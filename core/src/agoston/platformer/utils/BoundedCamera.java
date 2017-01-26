03c3347da9b507217b0016230544324211c0618f
Agoston Szepessy <agszepp@gmail.com>
2017-01-25 20:45:48 -0500

    Added documentation and fixes to BoundedCamera

--- core/src/agoston/platformer/utils/BoundedCamera.java
+++ core/src/agoston/platformer/utils/BoundedCamera.java
@@ -8,51 +8,89 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class BoundedCamera extends OrthographicCamera
 {
-	private float xBound;
-	private float yBound;
+	private float rightXBound;
+	private float topYBound;
+	private float leftXBound;
+	private float bottomYBound;
 
+	/**
+	 * Creates a <code>BoundedCamera</code> with a right <code>x</code> boundary of <code>1000</code> and
+	 * a top <code>y</code> of <code>1000</code>.
+	 */
 	public BoundedCamera()
 	{
 		super();
 
-		setBounds(1000, 1000);
+		setBounds(0, 0, 1000, 1000);
 	}
 
+	/**
+	 * Creates a <code>BoundedCamera</code> with an <code>rightXBound</code> of <code>1000</code> and
+	 * a <code>topYBound</code> of <code>1000</code>, and specified viewport width and viewport height.
+	 *
+	 * @param viewportWidth Width of the viewport
+	 * @param viewportHeight Height of the viewport
+	 */
 	public BoundedCamera(float viewportWidth, float viewportHeight)
 	{
 		super(viewportWidth, viewportHeight);
-		setBounds(1000, 1000);
+		setBounds(0, 0, 1000, 1000);
 	}
 
-	public BoundedCamera(float viewportWidth, float viewportHeight, float xBound, float yBound)
+	/**
+	 * Creates a <code>BoundedCamera</code> with specified values.
+	 * @param viewportWidth Width of the viewport
+	 * @param viewportHeight Height of the viewport
+	 * @param rightXBound The boundary of the camera on the <code>x</code> axis
+	 * @param topYBound The boundary of the camera on the <code>y</code> axis
+	 */
+	public BoundedCamera(float viewportWidth, float viewportHeight, float leftXBound,
+						 float rightXBound, float bottomYBound, float topYBound)
 	{
 		super(viewportWidth, viewportHeight);
-		setBounds(xBound, yBound);
+		setBounds(leftXBound, bottomYBound, rightXBound, topYBound);
 	}
 
-	public void setBounds(float x, float y)
+	/**
+	 * Sets the boundaries of the camera.
+	 * @param leftX the left <code>x</code> axis boundary
+	 * @param bottomY the bottom <code>y</code> axis boundary
+	 * @param rightX the right <code>x</code> boundary
+	 * @param topY the top <code>y</code> boundary
+	 */
+	public void setBounds(float leftX, float bottomY, float rightX, float topY)
 	{
-		xBound = x;
-		yBound = y;
+		leftXBound = leftX;
+		rightXBound = rightX;
+		bottomYBound = bottomY;
+		topYBound = topY;
 	}
 
+	/**
+	 * Checks if the camera is past its bounds and puts it back if it's out of bounds.
+	 */
 	public void checkBounds()
 	{
-		if(this.position.x < 0)
+		// check left x boundary
+		if(position.x - viewportWidth / 2 < leftXBound)
 		{
-			this.position.x = 0;
+			this.position.x = leftXBound;
 		}
-		else if(this.position.x > xBound)
+		// check right boundary
+		else if(position.x + viewportWidth / 2 > rightXBound)
 		{
-			this.position.x = xBound - this.viewportWidth / 2;
+			position.x = rightXBound - viewportWidth / 2;
 		}
-		if(this.position.y < 0)
+
+		// check bottom y boundary
+		if(position.y - viewportHeight / 2 < bottomYBound)
 		{
-			this.position.y = this.viewportHeight;
+			position.y = viewportHeight;
 		}
-		else if(this.position.y > this.yBound)
+		// check top y boundary
+		else if(position.y + viewportHeight / 2 > topYBound)
 		{
-			this.position.y = this.yBound;
+			position.y = topYBound;
 		}
 	}
 }
